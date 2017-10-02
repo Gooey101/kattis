93c920e6d6005001c18d4e0d4f4fde02219b8b4f
Christopher Gu <clg2n4@mst.edu>
2017-10-02 02:07:15 -0500

    Update pipeline.cpp

--- pipeline.cpp
+++ pipeline.cpp
@@ -10,7 +10,6 @@ int main() {
 
 	typedef long * LongPtr;
 	LongPtr delays = new long[N];
-	LongPtr combination = new long[N];
 	long currentCombination = 0;
 	long cumulativeDelay = 0;
 	long max = 0;
@@ -24,13 +23,12 @@ int main() {
 	for (long n = 0; n < N; n++) {
 		if (delays[n] + cumulativeDelay <= max) {
 			cumulativeDelay += delays[n];
-			combination[n] = currentCombination;
 		} else {
 			cumulativeDelay = delays[n];
 			currentCombination++;
 		}
 	}
 	cout << (currentCombination + 1) << endl;
-	
+
 	return 0;
 }
